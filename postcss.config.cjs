ecf1ea3f36e6db7a3347c192cd8c305dd97d154d
tshipman44 <tshipman@gmail.com>
2025-07-16 23:31:43 -0700

    nightmarefuel

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,8 @@
 // postcss.config.cjs   ← make sure this is the ONLY PostCSS config
+const tailwindcss  = require('@tailwindcss/postcss');
+const autoprefixer = require('autoprefixer');
+
+/** @type {import('postcss').Config} */
 module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},  // object key, NOT an array element
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss, autoprefixer],   // ← array of plugin FUNCTIONS
 };
