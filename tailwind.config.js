4a5bab7ed053cae4bf1c0b604d82525f9a9ee1f7
tshipman44 <tshipman@gmail.com>
2025-07-17 21:15:58 -0700

    test css fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './**/*.{js,jsx,ts,tsx}',   // ← scan everything everywhere
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
 
   safelist: [
